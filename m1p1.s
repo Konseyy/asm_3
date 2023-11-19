bc9370be6e9d4c4dfd3e88971469a59b5942b1e9
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-19 16:26:03 +0200

    types

--- m1p1.s
+++ m1p1.s
@@ -1,22 +1,25 @@
+.text
 .align 2
+.global m1p1
+.type m1p1, %function
 @ r0 = buffer address
-mov r4, #0 @ Holds whether last character was a space
+  mov r4, #0 @ Holds whether last character was a space
 loop:
   mov r1, r0 @ Copy buffer address to r1
   ldrb r2, [r1], #1 @ Read next byte from buffer
   cmp r2, #0 @ Check if end of string
   bxeq lr @ If end of string, return
-  @ Check if this character is a letter
+@ Check if this character is a letter
   cmp r2, #65
   blt loop @ if less than 65, this is not a letter
-  cmp r2, #122 
+  cmp r2, #122
   bgt loop @ if greater than 122, this is not a letter
-  @ check if r2 > 90 && r2 < 97
+@ check if r2 > 90 && r2 < 97
   cmp r2, #90
-  @store whether grater than 90
-  @store whether less than 97
-  @if both are true, this is not a letter
-  @@@@@@ TODO
+@store whether grater than 90
+@store whether less than 97
+@if both are true, this is not a letter
+@@@@@@ TODO
   cmp r0, r1 @ Check if this is the first character of the string
   beq uppercase @ If first character, this should be a capital letter
   cmp r2, #32 @ Check if this is a space
@@ -27,7 +30,7 @@ loop:
 
 uppercase:
   mov r4, #0
-  @transform this to uppercase if it already isnt
+@transform this to uppercase if it already isnt
   cmp r2, #96
   subgt r2, r2, #32
   strb r2, [r1]
@@ -37,7 +40,7 @@ whitespace:
   b loop
 lowercase:
   mov r4, #0
-  @transform this to lowercase if it already isnt
+@transform this to lowercase if it already isnt
   cmp r2, #91
   addlt r2, r2, #32
   strb r2, [r1]
