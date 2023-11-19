4d8a729b4b413b79bcd7d2cb695b304da15260d9
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-19 16:42:59 +0200

    asd

--- m1p1.s
+++ m1p1.s
@@ -11,7 +11,7 @@ loop:
   ldrb r2, [r5], #1 @ Read next byte from buffer
   cmp r2, #0 @ Check if end of string
   mov r0, r2 @ Load next byte into r0
-  ldr r1, .format_address @ Load format string into r1
+  ldr r1, f_1 @ Load format string into r1
   b printf @ Print next character
   bxeq lr @ If end of string, return
 @ Check if this character is a letter
@@ -52,9 +52,5 @@ lowercase:
   b loop
 
 @ Format string for printf
-.format_address:
-  .word format_string
-
-.data
-.format_string:
-  .asciz "%c"
+f_1:    .word format
+format: .asciz "%c"
