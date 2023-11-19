0b4cbe05547f41679af904b3f9d0c8f894ef61be
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-19 17:15:13 +0200

    asd

--- m1p1.s
+++ m1p1.s
@@ -5,7 +5,7 @@
 
 m1p1:
 @ r0 = buffer address
-  mov r4, #0 @ Holds whether last character was a space
+  mov r4, #0 @ Holds whether last character was a seperator
   mov r5, r0 @ Copy buffer address to r5
 loop:
   ldrb r2, [r5], #0 @ Read next byte from buffer
@@ -17,7 +17,13 @@ loop:
   cmp r2, #0 @ Check if end of string
   bxeq lr @ If end of string, return
   cmp r2, #32 @ Check if this is a space
-  beq whitespace @ If space, go to appropriate label
+  beq seperator @ If space, go to appropriate label
+  cmp r2, #9 @ Check if this is a tab
+  beq seperator @ If tab, go to appropriate label
+  cmp r2, #10 @ Check if this is a newline
+  beq seperator @ If newline, go to appropriate label
+  cmp r2, #13 @ Check if this is a carriage return
+  beq seperator @ If carriage return, go to appropriate label
 @ Check if this character is a letter
   cmp r2, #65
   blt not_letter @ if less than 65, this is not a letter
@@ -42,7 +48,7 @@ uppercase:
   subgt r2, r2, #32
   strb r2, [r5]
   b finish_char
-whitespace:
+seperator:
   mov r4, #1
   b finish_char
 lowercase:
