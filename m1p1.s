61073c0291804b8fbb15ef831c9cc39d01e8cace
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-11-19 16:38:12 +0200

    s

--- m1p1.s
+++ m1p1.s
@@ -7,9 +7,12 @@ m1p1:
 @ r0 = buffer address
   mov r4, #0 @ Holds whether last character was a space
 loop:
-  mov r1, r0 @ Copy buffer address to r1
-  ldrb r2, [r1], #1 @ Read next byte from buffer
+  mov r5, r0 @ Copy buffer address to r5
+  ldrb r2, [r5], #1 @ Read next byte from buffer
   cmp r2, #0 @ Check if end of string
+  ldr r0, r2 @ Load next byte into r0
+  ldr r1, format_string @ Load format string into r1
+  bx printf @ Print next character
   bxeq lr @ If end of string, return
 @ Check if this character is a letter
   cmp r2, #65
@@ -18,12 +21,11 @@ loop:
   bgt loop @ if greater than 122, this is not a letter
 @ check if r2 > 90 && r2 < 97
   cmp r2, #90
-  bx lr
 @store whether grater than 90
 @store whether less than 97
 @if both are true, this is not a letter
 @@@@@@ TODO
-  cmp r0, r1 @ Check if this is the first character of the string
+  cmp r0, r5 @ Check if this is the first character of the string
   beq uppercase @ If first character, this should be a capital letter
   cmp r2, #32 @ Check if this is a space
   beq whitespace @ If space, go to appropriate label
@@ -36,7 +38,7 @@ uppercase:
 @transform this to uppercase if it already isnt
   cmp r2, #96
   subgt r2, r2, #32
-  strb r2, [r1]
+  strb r2, [r5]
   b loop
 whitespace:
   mov r4, #1
@@ -46,5 +48,8 @@ lowercase:
 @transform this to lowercase if it already isnt
   cmp r2, #91
   addlt r2, r2, #32
-  strb r2, [r1]
+  strb r2, [r5]
   b loop
+
+.format_string:
+  .asciz "%c"
